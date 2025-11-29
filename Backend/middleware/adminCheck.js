7cd44736eb2dcdd16bfb50ec48cd416576cf99ff
Sohreet <projectsthapar@gmail.com>
2025-11-29 15:58:08 +0530

    Delete Backend directory

--- Backend/middleware/adminCheck.js
+++
@@ -1,9 +0,0 @@
-export default function adminCheck(req, res, next) {
-  const adminEmails = ["sohabrar10@gmail.com"];
-
-  if (!req.user || !adminEmails.includes(req.user.email)) {
-    return res.status(403).json({ error: "Admins only" });
-  }
-
-  next();
-}
