b64d25a71cbb5335a0e82fc927e209371165143c
Sohreet <projectsthapar@gmail.com>
2025-11-30 23:51:54 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-  title: String,
+const productSchema = new mongoose.Schema({
+  name: String,
   price: Number,
-  description: String,
+  pricePerMeter: Number,
   category: String,
-  isBestDeal: Boolean,
-  isSale: Boolean,
-  minMetres: Number,
-  maxMetres: Number,
-  stock: Number,
-  images: [String],   // ex: /uploads/images/abc.jpg
-  video: String       // ex: /uploads/videos/video.mp4
-}, { timestamps: true });
+  
+  description: String,
 
-module.exports = mongoose.model("Product", ProductSchema);
+  stock: { type: Number, default: 10 },
+
+  images: { type: [String], default: [] },
+  video: { type: String, default: "" },
+
+  sale: { type: Boolean, default: false },
+
+  orderCount: { type: Number, default: 0 },
+
+  addedAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Product", productSchema);
