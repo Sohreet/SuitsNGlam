b0ee24e7c652b1d6fa12c89761dd118f15a5e22c
Sohreet <projectsthapar@gmail.com>
2025-11-30 16:34:15 +0530

    Update multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -1,7 +1,7 @@
 const multer = require("multer");
 const path = require("path");
 
-// Storage setup
+// Storage location
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     if (file.mimetype.startsWith("image/")) {
@@ -9,7 +9,7 @@ const storage = multer.diskStorage({
     } else if (file.mimetype.startsWith("video/")) {
       cb(null, "uploads/videos");
     } else {
-      cb(null, "uploads/others");
+      cb(null, "uploads");
     }
   },
   filename: function (req, file, cb) {
