a0001ba8c720f1839bef292574730e47592f3b9f
Sohreet <projectsthapar@gmail.com>
2025-12-01 00:06:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+
 // Serve EVERYTHING inside /public
 app.use(express.static(path.join(__dirname, "public")));
 
