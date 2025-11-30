4a46e6572226bdbf75eec620abc36a3f4954c473
Sohreet <projectsthapar@gmail.com>
2025-12-01 01:30:44 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
+const productSchema = new mongoose.Schema({
   name: String,
   price: Number,
   category: String,
   description: String,
   stock: Number,
-  images: [String],
-  video: String,
+  images: [String],     // base64 strings
+  video: String,        // base64 string (optional)
   sale: Boolean,
   createdAt: Date
 });
 
-module.exports = mongoose.model("Product", ProductSchema);
+module.exports = mongoose.model("Product", productSchema);
